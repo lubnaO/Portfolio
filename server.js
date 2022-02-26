de52fdd647a6e76e091afc238264bc59cd5c5663
lubnaO <lubnao115@gmail.com>
2022-02-26 20:55:37 +0300

    add server file

---
+++ server.js
@@ -0,0 +1,19 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("./Data/projects.json");
+const middlewares = jsonServer.defaults({
+    static: "./build",
+})
+
+const port = process.env.PORT || 4000;
+server.use(middlewares);
+server.use(
+    jsonServer.rewriter({
+        "/api/*": "/$1",
+    })
+)
+
+server.use(router);
+server.listen(port, ()=> {
+    console.log(`server is run on ${port}`);
+})
